2d640d2ba65525dcf1530be0919123c30b27c941
Mvng-ii <victor.mbugu@strathmore.edu>
2024-07-02 08:57:03 +0300

    Email Verification

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,13 +3,6 @@
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
       {{ __('Admin Dashboard') }}
     </h2>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <x-responsive-nav-link :href="route('logout')"
-                onclick="event.preventDefault();
-                this.closest('form').submit();">
-            {{ __('Logout') }}
-        </x-responsive-nav-link>
     </form>
 
   </x-slot>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\LockScreenController;
+use App\Http\Middleware\CheckIfLocked;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,5 +19,20 @@ Route::middleware([
     })->name('dashboard');
 });
 
+Route::middleware(['auth:sanctum'])->group(function (){
+    // Lock screen routes
+    Route::get('/lock', [LockScreenController::class, 'show'])->name('lock');
+    Route::post('/unlock', [LockScreenController::class, 'unlock'])->name('unlock');
+
+    // Resources
+    Route::resource('tasks', Controllers\TaskController::class);
+    Route::resource('users', Controllers\UserController::class);
+});
+
+Route::get('/testroute', function() {
+    $name = "Funny Coder";
+    Mail::to("victor.mbugu@strathmore.edu")->send(new MyProjectEmail($name));
+});
+
 route::get('/home',[HomeController::class, 'index']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+route::get('/adminpage',[HomeController::class, 'page'])->middleware(['auth', 'admin']);
